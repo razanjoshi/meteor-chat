2ac580b77d28057bd11ca35ea81a3b26bf9abee7
razanjoshi <rajan4ualwz@hotmail.com>
2016-11-09 16:07:21 +0545

    Show username if signed in.

--- client/client.js
+++ client/client.js
@@ -15,8 +15,13 @@ if (Meteor.isClient) {
     Template.input.events = {
       'keydown input#message' : function (event) {
         if (event.which == 13) { // 13 is the enter key event
-          if (Meteor.user)
-            var name = Meteor.user().username;
+          if (Meteor.user())
+            if (Meteor.user().username){
+              var name = Meteor.user().username;
+            }
+            else {
+              var name = Meteor.user().profile.name;
+            }
           else
             var name = 'Anonymous';
           var message = document.getElementById('message');
